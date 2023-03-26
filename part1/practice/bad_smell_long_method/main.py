cada9b742d16b337373af7bc81fb1a75f206c0fd
Asm <destroasm@gmail.com>
2023-03-26 18:21:53 +0300

    f

--- part1/practice/bad_smell_long_method/main.py
+++ part1/practice/bad_smell_long_method/main.py
@@ -18,46 +18,25 @@ def get_users_list():
     for line in csv.split('\n'):
         name, age = line.split(';')
         data.append({'name': name, 'age': int(age)})
+    return data
 
+def sort_by_age(data):
     # Сортировка по возрасту по возрастанию
-    _new_data = []
-    used_person = set()
-    minimum_age_person = None
-    while len(used_person) != len(data):
-        if minimum_age_person is None:
-            for person in data:
-                if minimum_age_person is None:
-                    minimum_age_person = person
-                else:
-                    if person['name'] in used_person:
-                        continue
-                    else:
-                        if person['age'] < minimum_age_person['age']:
-                            minimum_age_person = person
-            _new_data.append(minimum_age_person)
-            used_person.add(minimum_age_person['name'])
-        local_minimum = None
-        for person in data:
-            if person['name'] in used_person:
-                continue
-            else:
-                if not local_minimum is None:
-                    if person['age'] < local_minimum['age']:
-                        local_minimum = person
-                else:
-                    local_minimum = person
-        _new_data.append(local_minimum)
-        used_person.add(local_minimum['name'])
+    new_data = sorted(data, key=lambda x: x['age'])
+    return new_data
 
-    # Фильтрация
-    result_data = []
-    for person in _new_data:
-        if person['age'] < 10:
-            continue
-        else:
-            result_data.append(person)
+def filter_age(data):
+    # # Фильтрация
+    result_data = list(filter(lambda k: k['age'] > 10, data))
+
     return result_data
+
+def main():
+    res = get_users_list()
+    res = sort_by_age(res)
+    res = filter_age(res)
+    return res
 
 
 if __name__ == '__main__':
-    print(get_users_list())
+    print(main())
--- part1/practice/bad_smell_long_parameter_list/main.py
+++ part1/practice/bad_smell_long_parameter_list/main.py
@@ -11,40 +11,39 @@
 
 
 class Unit:
-    def move(self, field, x_coord, y_coord, direction, is_fly, crawl, speed = 1):
 
-        if is_fly and crawl:
-            raise ValueError('Рожденный ползать летать не должен!')
+    def __init__(self, state, speed=1):
+        self.state = state
+        self.speed = speed
+        self.new_y = 0
+        self.new_x = 0
 
-        if is_fly:
-            speed *= 1.2
-            if direction == 'UP':
-                new_y = y_coord + speed
-                new_x = x_coord
-            elif direction == 'DOWN':
-                new_y = y_coord - speed
-                new_x = x_coord
-            elif direction == 'LEFT':
-                new_y = y_coord
-                new_x = x_coord - speed
-            elif direction == 'RIGTH':
-                new_y = y_coord
-                new_x = x_coord + speed
-        if crawl:
-            speed *= 0.5
-            if direction == 'UP':
-                new_y = y_coord + speed
-                new_x = x_coord
-            elif direction == 'DOWN':
-                new_y = y_coord - speed
-                new_x = x_coord
-            elif direction == 'LEFT':
-                new_y = y_coord
-                new_x = x_coord - speed
-            elif direction == 'RIGTH':
-                new_y = y_coord
-                new_x = x_coord + speed
+    def determine_speed(self):
+        if self.state == 'fly':
+            self.speed *= 1.2
+        elif self.state == 'crawl':
+            self.speed *= 0.5
+        else:
+            raise ValueError('нельзя летать и ползать')
+        return self.speed
 
-            field.set_unit(x=new_x, y=new_y, unit=self)
+    def move(self, x_coord, y_coord, direction):
+
+        if direction == 'UP':
+            self.new_y = y_coord + self.speed
+            self.new_x = x_coord
+        elif direction == 'DOWN':
+            self.new_y = y_coord - self.speed
+            self.new_x = x_coord
+        elif direction == 'LEFT':
+            self.new_y = y_coord
+            self.new_x = x_coord - self.speed
+        elif direction == 'RIGTH':
+            self.new_y = y_coord
+            self.new_x = x_coord + self.speed
+
+
+    def set_field(self, field):
+        field.set_unit(x=self.new_x, y=self.new_y, unit=self)
 
 #     ...
